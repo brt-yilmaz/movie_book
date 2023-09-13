ea20a7f5a1dc509f6282f43cc23f20b07e2c7739
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-09-13 23:06:21 +0200

    Backend-Feat: Implement update Password

--- Backend/controllers/authController.ts
+++ Backend/controllers/authController.ts
@@ -230,3 +230,25 @@ export const resetPassword = catchAsync(async (req, res, next) => {
   // 4) Log the user in, send JWT
   createSendToken(user, 200, req, res);
 })
+
+export const updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user?.id).select('+password');
+  
+  // 2) Check if POSTed current password is correct
+  if (!(await user?.correctPassword?.(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  if(user) {
+    // 3) If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    await user.save();
+    // Don't use findByIdAndUpdate here
+
+    // 4) Log user in, send JWT
+    createSendToken(user, 200, req, res);
+  }
+
+})
--- Backend/routes/userRouter.ts
+++ Backend/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup , login, logout, protect, forgotPassword, resetPassword} from '../controllers/authController';
+import {signup , login, logout, protect, forgotPassword, resetPassword, updatePassword} from '../controllers/authController';
 
 const router = express.Router();
 
@@ -12,6 +12,8 @@ router.patch('/resetPassword/:token', resetPassword);
 
 // Protect all routes after this middleware
 router.use(protect);
+
+router.patch('/updateMyPassword', updatePassword);
 
 
 export default router;
