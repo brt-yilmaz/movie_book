bc58735fb84002887bb5452893c4fc9e5af702f1
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-11-06 15:27:28 +0100

    Chore: Change api request stale time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      staleTime: 1000,
+      staleTime: 1000 * 60 * 60,
     },
   },
 });
