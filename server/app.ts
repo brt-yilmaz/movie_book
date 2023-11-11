c497286eaf34e3c7d4aad2829ba27b19a203da55
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-11-11 16:00:15 +0100

    Feat: Work Backend and Frontend in the same domain

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,4 @@
-import express , { Application, RequestHandler, Request } from "express";
-import cors from "cors";
+import express, { Application, RequestHandler, Request } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
@@ -7,37 +6,41 @@ import mongoSanitize from "express-mongo-sanitize";
 import compression from "compression";
 import AppError from "./utils/appError";
 import globalErrorHandler from "./utils/globalErrorHandler";
-import hpp from 'hpp'
+import hpp from "hpp";
 import xss from "xss";
 import cookieParser from "cookie-parser";
-import userRouter from './routes/userRouters'
-import path from "path";
-import reviewRouter from './routes/reviewRouters'
-import movieRouter from './routes/movieRouters'
-
-
+import userRouter from "./routes/userRouters";
+import reviewRouter from "./routes/reviewRouters";
+import movieRouter from "./routes/movieRouters";
 
 // CONFIGURATIONS
 
 const app: Application = express();
 // app.enable('trust proxy');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true,  limit: '10kb'}));
-app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'https://www.google-analytics.com'],
+      connectSrc: ["'self'", 'https://api.themoviedb.org'],
+      imgSrc: ["'self'", 'data:', 'https://www.google-analytics.com', 'https://image.tmdb.org'],
+    },
+  },
+}));
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
+app.use("/", express.static("./dist"));
+app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.use(morgan("common"));
-app.options("*", cors());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
-
 
 var html = xss('<script>alert("xss");</script>');
 
@@ -45,14 +48,13 @@ var html = xss('<script>alert("xss");</script>');
 app.use(
   hpp({
     whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price'
-    ]
-
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
   })
 );
 
@@ -72,22 +74,21 @@ const limiter: RequestHandler = rateLimit({
 });
 
 app.use("/api", limiter);
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 app.use(cookieParser());
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "pug");
+// app.set("views", path.join(__dirname, "/views"));
 
 // ROUTES
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/movies', movieRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/movies", movieRouter);
 
-
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
